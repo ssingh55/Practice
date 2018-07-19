8bff25a74e495265759aaee5f6a2472b6600d503
Shubham Singh <shubham.singh@mountblue.io>
2018-07-19 17:27:14 +0530

    6th part

--- src/App.js
+++ src/App.js
@@ -1,39 +1,21 @@
 import React, { Component } from 'react';
 import './App.css';
+import Parent from './components/parentToChild/parent'
 
 class App extends Component {
-  state = {
-    name: 'techasas'
+  state={
+    title: 'placeholder title'
   }
-
-  changeName1 = () => {
-    this.setState({
-        name: 'something different'
-    })
-  }
-  changeName = (newName)=>{
-    this.setState({
-      name: newName
-    })
-  }
-
-  changeNameFromInput = (event)=>{
-    this.setState({
-      name: event.target.value
-    })
+  changeTheWorld = (newTitle)=>{
+    this.setState({title:newTitle})
   }
 
   render() {
     return (
       <div className="App">
-      <br/>
-      <button onClick={this.changeName1}>Change button1</button>
-      <button onClick={()=>this.changeName('Awesome sasas')}>Change button2</button>
-      <button onClick={this.changeName.bind(this,'wesome bind')}>Change button3</button>
-      <br/> <br/>
-      <input type="text" onChange={this.changeNameFromInput} value={this.state.name}/>
-      <br/>
-      <div>{this.state.name}</div>
+        <Parent changeTheWorldEvent={this.changeTheWorld.bind(this,'new World')} 
+         keepTheWorldEvent={this.changeTheWorld.bind(this,'same World')} 
+         title={this.state.title}/> 
       </div>
     );
   }
