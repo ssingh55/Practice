42568d255dce1b0de17c76d0d65f7222a809c3db
Shubham Singh <shubham.singh@mountblue.io>
2018-07-19 17:02:55 +0530

    5th part

--- src/App.js
+++ src/App.js
@@ -1,13 +1,39 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
-import Users from './users/Users';
 
 class App extends Component {
+  state = {
+    name: 'techasas'
+  }
+
+  changeName1 = () => {
+    this.setState({
+        name: 'something different'
+    })
+  }
+  changeName = (newName)=>{
+    this.setState({
+      name: newName
+    })
+  }
+
+  changeNameFromInput = (event)=>{
+    this.setState({
+      name: event.target.value
+    })
+  }
+
   render() {
     return (
       <div className="App">
-        <Users title="Users list"/>
+      <br/>
+      <button onClick={this.changeName1}>Change button1</button>
+      <button onClick={()=>this.changeName('Awesome sasas')}>Change button2</button>
+      <button onClick={this.changeName.bind(this,'wesome bind')}>Change button3</button>
+      <br/> <br/>
+      <input type="text" onChange={this.changeNameFromInput} value={this.state.name}/>
+      <br/>
+      <div>{this.state.name}</div>
       </div>
     );
   }
